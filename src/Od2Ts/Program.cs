c81909d42383b472d4f1b132d5ce29f0ca90a5df
Diego van Haaster <diegomvh@gmail.com>
2018-02-21 22:28:03 -0300

    Console app

---
+++ src/Od2Ts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Od2Ts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
