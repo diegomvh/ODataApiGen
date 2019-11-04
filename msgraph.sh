22b0a02c15b87956cbf57015e2aac596ff298feb
Diego van Haaster <diegomvh@gmail.com>
2019-11-04 20:23:57 -0300

    Use docker image

--- msgraph.sh
+++ msgraph.sh
@@ -1,4 +1,4 @@
-docker run -it --rm -v ${PWD}:/local odataapigen \
+docker run -it --rm -v ${PWD}:/local diegomvh/odataapigen \
   Name=MsGraph \
   Metadata=https://graph.microsoft.com/v1.0/\$metadata \
   Output=/local
