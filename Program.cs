ba867def00d4271fadf15fbc3024b2b53d9f8258
Diego van Haaster <diegomvh@gmail.com>
2019-12-16 23:00:57 -0300

    Update settings

--- Program.cs
+++ Program.cs
@@ -29,7 +29,7 @@ namespace ODataApiGen
 
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("application.trippinmodel.json")
+                .AddJsonFile("application.json")
                 .AddCommandLine(args, new Dictionary<string, string>() {
                     {"-Name", "Name"},
                     {"-Metadata", "Metadata"},
