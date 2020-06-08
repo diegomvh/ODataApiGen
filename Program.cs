b5cf9a68fd3d84c48e39f88f5710bc45c007ca85
Diego van Haaster <diegomvh@gmail.com>
2020-06-08 16:08:45 -0300

    Use correct settings

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ namespace ODataApiGen
 
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("application.siuelements.json")
+                .AddJsonFile("application.json")
                 .AddCommandLine(args, new Dictionary<string, string>() {
                     {"-Name", "Name"},
                     {"-Metadata", "Metadata"},
