ca76bf1b149059aa530566575eacda48ed55a8bf
Diego van Haaster <diegomvh@gmail.com>
2020-12-03 22:48:44 -0300

    Version are option

--- Templates/Angular/ApiConfig.ts
+++ Templates/Angular/ApiConfig.ts
@@ -7,8 +7,10 @@ import { ApiConfig, EDM_PARSERS } from 'angular-odata';
 export const {{Name}} = {
   name: '{{Package.Name}}',
   serviceRootUrl: '{{Package.ServiceRootUrl}}',
-  version: '{{Package.Version}}',
   creation: new Date('{{Package.Creation | date: "o"}}'),
+  options: {
+    version: '{{Package.Version}}'
+  },
   schemas: [
     {% for schema in Package.Schemas %}{{schema.Name}}{% unless forloop.last %},
     {% endunless %}{% endfor %}
