51287525f9187f23856069f9f2c4daced346ca34
Diego van Haaster <diegomvh@gmail.com>
2021-04-28 07:43:47 -0300

    Remove Constant

--- Templates/Angular/Enum.ts
+++ Templates/Angular/Enum.ts
@@ -2,7 +2,6 @@
 {% for import in Imports %}import { {{import.Names | join: ", "}} } from '{{import.Path}}';
 {% endfor %}//#endregion
 
-export const ISFLAGS_{{Name | upcase}} = {{Flags}};
 export enum {{Name}} {
   //#region ODataApi Members
   {% for member in Members %}{{member}},
