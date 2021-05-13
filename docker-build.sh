86cfbdb41885beb1d9652691be13b58808090890
Diego van Haaster <diegomvh@gmail.com>
2021-05-13 00:04:19 -0300

    Docker build image

--- docker-build.sh
+++ docker-build.sh
@@ -8,4 +8,5 @@ TAG="${1}"
 REGISTRY="docker.io"
 
 docker build -t ${REGISTRY}/${IMAGE_NAME}:${TAG} -t ${REGISTRY}/${IMAGE_NAME}:latest .
-docker push ${REGISTRY}/${IMAGE_NAME}
+docker push ${REGISTRY}/${IMAGE_NAME}:${TAG}
+docker push ${REGISTRY}/${IMAGE_NAME}:latest
