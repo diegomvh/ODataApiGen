2069565b8bc02253fbb65405c4d38dbce7851710
Diego van Haaster <diegomvh@gmail.com>
2022-12-27 13:11:30 -0300

    Fix json File load

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ namespace ODataApiGen
 
       var builder = new ConfigurationBuilder()
           .SetBasePath(Directory.GetCurrentDirectory())
-          .AddJsonFile("application.mele.json")
+          .AddJsonFile("application.json")
           .AddCommandLine(args, new Dictionary<string, string>() {
             {"-Name", "Name"},
             {"-Metadata", "Metadata"},
