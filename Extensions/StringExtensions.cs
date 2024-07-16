c09e78523e2a3cc3fd6735d5b74d99df8a5a4f00
Diego van Haaster <diegomvh@gmail.com>
2024-07-16 11:47:29 -0300

    Dasherize

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -2,9 +2,14 @@ static class StringExtensions
 {
     public static string Dasherize(this string str)
     {
-        return string
-           .Concat(str
-           .Select((x, i) => char.IsUpper(x) && i != 0 ? $"-{x}" : x.ToString()))
-           .ToLower();
+        if (str.Length > 3) {
+            // Replace _ by -
+            str = str.Replace("_", "-");
+            // Replace Uppercase by -<Uppercase>
+            str = string.Concat(str.Select((x, i) => char.IsUpper(x) && i != 0 ? $"-{x}" : x.ToString()));
+            // Remove extra -
+            str = string.Join("-", str.Split("-", StringSplitOptions.RemoveEmptyEntries));
+        }
+        return str.ToLower();
     }
 }
